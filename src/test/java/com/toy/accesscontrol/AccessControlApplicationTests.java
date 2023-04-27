f4f798a744ae759a7e4fea2b3735f5d959fcdc17
TonyJev93 <tonyJev93@gmail.com>
2023-04-28 06:43:55 +0900

    init

---
+++ src/test/java/com/toy/accesscontrol/AccessControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toy.accesscontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessControlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
